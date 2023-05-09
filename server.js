bb43299e88e6241948f4a848768c0d25c020931a
James Browning <jabrowni@email.unc.edu>
2023-05-09 14:50:52 -0400

    lets try again

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ import express from 'express';
 import { rps, rpsls } from './lib/a03-jmsabrwnng/lib/rpsls.js';
 
 const app = express();
-const port = process.argv[2] || 5000;
+const port = process.argv.slice(2).find(arg => arg.startsWith('--port='))?.split('=')[1] || 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Default API endpoint
 app.get('/', (req, res) => {
@@ -16,46 +19,89 @@ app.get('/app/', (req, res) => {
 
 // Endpoint /app/rps/
 app.get('/app/rps/', (req, res) => {
-  res.json({ player: '(rock|paper|scissors)' });
+  const result = rps();
+  res.json(result);
 });
 
 // Endpoint /app/rpsls/
 app.get('/app/rpsls/', (req, res) => {
-  res.json({ player: '(rock|paper|scissors|lizard|spock)' });
-});
-
-// Endpoint /app/rps/play/
-app.get('/app/rps/play/', (req, res) => {
-  const { shot } = req.body;
-
-  if (!shot) {
-    res.status(400).json({ error: 'Missing required field: shot' });
-    return;
-  }
-
-  const result = rps(shot);
-  res.json(result); 
-});
-
-// Endpoint /app/rpsls/play/
-app.post('/app/rpsls/play/', (req, res) => {
-  const { shot } = req.body;
-  const result = rpsls(shot);
+  const result = rpsls();
   res.json(result);
 });
 
-// Endpoint /app/rpsls/play/(rock|paper|scissors)/
-app.get('/app/rpsls/play/:shot', (req, res) => {
-  const { shot } = req.params;
+// Endpoint /app/rps/play/
+/*app.get('/app/rps/play/', (req, res) => {
+  let shot
+  const randomNum = Math.random();
+  if (randomNum < 1 / 3) {
+    shot = 'rock';
+  } else if (randomNum < 2 / 3) {
+    shot = 'paper';
+  } else {
+    shot = 'scissors';
+  }
+  const result = rps(shot);
+  res.json(result);
+});*/
+
+// Endpoint /app/rps/play/ that accepts shot=(rock|paper|scissors) (URLEncoded) or {"shot":"(rock|paper|scissors)"} (JSON) as request bodies
+app.post('/app/rps/play/', (req, res) => {
+  // Extract the shot from the request body
+  const shot = req.body.shot || req.body;
+  
+  // Determine the result
+  const result = rps(shot)
+  
+  // Return the response
+  res.json(result);
+});
+
+// Endpoint /app/rpsls/play/ that accepts shot=(rock|paper|scissors|lizard|Spock) (URLEncoded) or {"shot":"(rock|paper|scissors|lizard|Spock)"} (JSON) as request bodies
+app.post('/app/rpsls/play/', (req, res) => {
+  // Extract the shot from the request body
+  const shot = req.body.shot || req.body;
+
+  // Determine the result
+  const result = rps(shot)
+
+  // Return the response
+  res.json(result);
+});
+
+// Endpoint /app/rpsls/play/
+/*app.get('/app/rpsls/play/', (req, res) => {
+  let shot
+  const randomNum = Math.random();
+  if (randomNum < 1 / 5) {
+    shot = 'rock';
+  } else if (randomNum < 2 / 5) {
+    shot = 'paper';
+  } else if (randomNum < 3 / 5) {
+    shot = 'scissors';
+  } else if (randomNum < 4 / 5) {
+    shot = 'lizard';
+  } else {
+    shot = 'Spock';
+  }
   const result = rpsls(shot);
+  res.json(result);
+});*/
+
+// Endpoint /app/rps/play/(rock|paper|scissors)/
+app.get('/app/rps/play/:shot', (req, res) => {
+  const result = rpsls(req.params.shot);
   res.json(result);
 });
 
 // Endpoint /app/rpsls/play/(rock|paper|scissors|lizard|spock)/
-app.get('/app/rpsls/play/:shot', (req, res) => {
-  const { shot } = req.params;
-  const result = rpsls(shot);
+app.post('/app/rpsls/play/:shot', (req, res) => {
+  const result = rpsls(req.params.shot);
   res.json(result);
+});
+
+// Endpoint for handling undefined routes
+app.use((req, res) => {
+  res.sendStatus(404);
 });
 
 // Start the server
